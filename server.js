7400cdcd5dce4be469cdd507536229168383732b
solomon-winner <solyalew3@gmail.com>
2023-12-28 12:39:19 +0300

    server

--- server.js
+++ server.js
@@ -5,7 +5,44 @@ import mysql from "mysql"
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "MY_DATA"
+})
+
+app.get('/', (req,res) => {
+    const sql = "SELECT * FROM StudentList";
+    db.query(sql, (err, result) => {
+        if(err) return res.json({Message: "Error inside Server"});
+        return res.json(result);
+    })
+})
+
+app.get('/read/:id', (req, res) => {
+    const sql = "SELECT * FROM StudentList WHERE ID = ?"
+        
+   const id = req.params.id;
+    console.log("the id is =======================>"+req.params.id)
+    db.query(sql, [id], (err,result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    })
+})
+
+app.post('/student',(req, res) => {
+    const sql = "INSERT INTO StudentList (Name, Email) VALUES (?)"
+   console.log(req.body)
+    const values = [req.body.name,
+                    req.body.email]
+        db.query(sql, [values], (err, result) => {
+            if(err) return res.json(err);
+            return res.json(result);
+        })
+})
 app.listen(8081, () => {
     console.log("I'm Listening!");
 })
